9f7e5b0c52aa0a1e9f98faa1aceeb93a38859f15
Patrick Carvalho <patrickcarvalho448@gmail.com>
2023-09-20 22:53:52 -0300

    gitignore

--- cmake-build-debug/CMakeFiles/ClassicAI.dir/src/ControlStrategy.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/ClassicAI.dir/src/ControlStrategy.cpp.o: \
- /home/patrick/Desktop/Projects/Classic-AI/src/ControlStrategy.cpp \
- /usr/include/stdc-predef.h \
- /home/patrick/Desktop/Projects/Classic-AI/include/ControlStrategy.h
